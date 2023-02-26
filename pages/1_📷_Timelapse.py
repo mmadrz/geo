b9e56b76efd0c09cb49e6264c5defd3c9751b015
mmadrz <mmadrzfathi@gmail.com>
2023-02-26 13:12:47 +0330

    json

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -10,13 +10,24 @@ import geemap.colormaps as cm
 import geemap.foliumap as geemap
 from datetime import date
 from shapely.geometry import Polygon
+import json
 
 st.set_page_config(layout="wide")
 warnings.filterwarnings("ignore")
 
 
+
 json_data = st.secrets["json_data"]
 service_account = st.secrets["service_account"]
+# Preparing values
+json_object = json.loads(json_data, strict=False)
+service_account = json_object['client_email']
+json_object = json.dumps(json_object)
+# Authorising the app
+credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
+ee.Initialize(credentials)
+
+
 
 @st.cache_data
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
